007350bf34157eee9f5dce75b419525ae97f49ea
Soumen-dev-ux <soumenpore0109@gmail.com>
2026-08-03 16:04:17 +0530

    nav updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import NoiseBackground from "@/components/noise-dark-blue-gradient-with-squares"
 import GsapInitializer from "@/components/gsap-initializer"
+import Head from "next/head"
 
 export const metadata: Metadata = {
   title: "Soumen Pore",
@@ -18,6 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
       <body className={`font-sans antialiased text-foreground selection:bg-primary/30 selection:text-primary`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
           <GsapInitializer />
